af266763ab2b11f2f86d8611e85a23d49115f6d0
jeanpah <jeanpah@hotmail.com>
2017-05-23 12:10:20 -0500

    inicializacion del repositorio

---
+++ js/routes.js
@@ -0,0 +1,28 @@
+angular.module('app.routes', [])
+        .config(function ($stateProvider, $urlRouterProvider) {
+            $stateProvider
+                    .state('dbasselection', {
+                        url: '/selectDbas',
+                        templateUrl: 'templates/selectDbas.html',
+                        controller: 'constructTestCtrl'
+                    })
+                    .state('selectQuestions', {
+                        url: '/selectQuestions',
+						            cache: false,
+                        templateUrl: 'templates/selectQuestions.html',
+                        controller: 'selectQuestionsCtrl'
+                    })
+                    .state('askaquestion', {
+                        url: '/answerTest',
+                        cache: false,
+                        templateUrl: 'templates/askQuestion.html',
+                        controller: 'askTestCtrl'
+                    })
+                    .state('answersFeedback', {
+                        url: '/answersFeedback',
+                        cache: false,
+                        templateUrl: 'templates/answersFeedback.html',
+                        controller: 'feedbackTestCtrl'
+                    })
+            $urlRouterProvider.otherwise('/selectDbas')
+        });
